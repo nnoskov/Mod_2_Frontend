1d71aee266193726a821244892fde10036986b0f
nnoskov <hhb@bk.ru>
2024-12-13 23:57:59 +0300

    Conditional rendering and event handling

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,22 +1,48 @@
 import { useState } from 'react';
 
 export const MyComponent = () => {
-	const [obj, setObj] = useState({ a: 10, b: 30, c: 50 });
+	const products = [
+		{ id: 'asdf', name: 'Penal' },
+		{ id: 'ddss', name: 'Iron' },
+	];
 
-	//obj.a = 23; is not possible in react!!
+	const [name, setName] = useState(null);
+	const [keyPressed, setKeyPressed] = useState(null); // Для хранения уникального ключа
+	const [value, setValue] = useState(0);
+	const [showText, setShowText] = useState(false);
 
+	const onClicker = (id, name) => {
+		setName(name);
+		setKeyPressed(id); // Устанавливаем уникальный ключ
+	};
+
+	const increment = () => {
+		setValue((updatedValue) => updatedValue + 1);
+		setValue((updatedValue) => updatedValue + 1);
+		setValue((updatedValue) => updatedValue + 1);
+		if (value > 15) setShowText(true);
+	};
+
+	const span = (
+		<div>
+			<span>Selected Product: {name}</span>
+		</div>
+	);
 	return (
 		<>
-			<button
-				onClick={(e) => {
-					setObj({ ...obj, a: e.clientX });
-				}}
-			>
-				Set Value
-			</button>
+			{showText && span}
+			<div>
+				<span>Key Pressed: {keyPressed}</span>
+			</div>
+			{products.map(({ id, name }) => (
+				<button key={id} onClick={() => onClicker(id, name)} style={{ margin: '5px' }}>
+					{name}
+				</button>
+			))}
 			<br />
-			Obj: {JSON.stringify(obj)}
+			<span>{value}</span>
 			<br />
+			<button onClick={increment}>{showText ? '+1' : '+6'}</button>
 		</>
 	);
 };
