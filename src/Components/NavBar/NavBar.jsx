dd12af100424eb961f254f9baa8cc5ec2f4033d2
nnoskov <hhb@bk.ru>
2024-12-17 23:43:18 +0300

    Styles for global

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router';
-import './NavBar.module.css';
+import './NavBar.css';
 
 export const NavBar = () => {
 	return (
 		<nav>
 			<ul>
 				<li>
-					<Link to="task_1">Task #1</Link>
+					<Link to="/task_1">Task #1</Link>
 				</li>
 				<li>
-					<Link to="task_2">Task #2</Link>
+					<Link to="/task_2">Task #2</Link>
 				</li>
 				<li>
-					<Link to="task_3">Task #3</Link>
+					<Link to="/task_3">Task #3</Link>
 				</li>
 			</ul>
 		</nav>
