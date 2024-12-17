3dc31116fb5277244b9825156f74ac0cbff8d0e8
nnoskov <hhb@bk.ru>
2024-12-18 00:05:08 +0300

    base link has been added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Task_1 } from './Components/Task_1/Task_1';
-import { Task_3 } from './Components/Task_3/Task_3';
 import { Task_2 } from './Components/Task_2/Task_2';
+import { Task_3 } from './Components/Task_3/Task_3';
 import { NavBar } from './Components/NavBar/NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router';
+
+const base = import.meta.env.BASE_URL;
 
 export const App = () => {
 	return (
@@ -11,9 +13,9 @@ export const App = () => {
 				<div>
 					<NavBar />
 					<Routes>
-						<Route path="/task_1" element={<Task_1 />} />
-						<Route path="/task_2" element={<Task_2 />} />
-						<Route path="/task_3" element={<Task_3 />} />
+						<Route path={`${base}task_1`} element={<Task_1 />} />
+						<Route path={`${base}task_2`} element={<Task_2 />} />
+						<Route path={`${base}task_3`} element={<Task_3 />} />
 						<Route path="*" element={<p>Select the task</p>} />
 					</Routes>
 				</div>
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,18 +1,20 @@
 import { Link } from 'react-router';
 import './NavBar.css';
 
+const base = import.meta.env.BASE_URL;
+
 export const NavBar = () => {
 	return (
 		<nav>
 			<ul>
 				<li>
-					<Link to="/task_1">Task #1</Link>
+					<Link to={`${base}task_1`}>Task #1</Link>
 				</li>
 				<li>
-					<Link to="/task_2">Task #2</Link>
+					<Link to={`${base}task_2`}>Task #2</Link>
 				</li>
 				<li>
-					<Link to="/task_3">Task #3</Link>
+					<Link to={`${base}task_3`}>Task #3</Link>
 				</li>
 			</ul>
 		</nav>
