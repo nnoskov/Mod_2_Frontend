0ce8b3f5179e80ace2a950b991c4498280f33dbb
nnoskov <hhb@bk.ru>
2025-01-02 01:31:48 +0300

    root directory of Game has been deleted.

--- src/Components/Lesson3/task_1/task_1.jsx
+++ src/Components/Lesson3/task_1/task_1.jsx
@@ -1,5 +1,5 @@
 //import styles from './task_1.module.css';
-import Game from './game/gameComponents/game/gameContainer';
+import Game from './gameComponents/game/gameContainer';
 
 export const Task_1 = () => {
 	return <Game />;
